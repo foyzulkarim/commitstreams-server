3e8f90b9bd3557b50138acd1198659c4f7328ca6
Foyzul Karim <foyzulkarim@gmail.com>
2024-04-25 14:04:26 +1000

    - Remove test environment configuration files and update gitignore - Added argument passing when running the commands - Configure loggers depending on the environments

--- src/configs/config.schema.js
+++ src/configs/config.schema.js
@@ -1,12 +1,21 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 const schema = Joi.object({
   NODE_ENV: Joi.string()
-    .valid("development", "production", "test")
-    .default("development"),
+    .valid('development', 'production', 'test')
+    .default('development'),
   MONGODB_URI: Joi.string().required(),
   RATE: Joi.number().min(0).required(),
   PORT: Joi.number().min(1000).default(4000),
+  // LOGGLY is required when NODE_ENV is production
+  LOGGLY_TOKEN: Joi.string().when('NODE_ENV', {
+    is: 'production',
+    then: Joi.required(),
+  }),
+  LOGGLY_SUBDOMAIN: Joi.string().when('NODE_ENV', {
+    is: 'production',
+    then: Joi.required(),
+  }),
 });
 
 module.exports = schema;
