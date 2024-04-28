928198a82d4c1d58fb9620d9eb8358339a0c92bc
Foyzul Karim <foyzulkarim@gmail.com>
2024-04-28 12:49:40 +1000

    Implemented github authentication (refactor need)

--- src/configs/config.schema.js
+++ src/configs/config.schema.js
@@ -16,6 +16,16 @@ const schema = Joi.object({
     is: 'production',
     then: Joi.required(),
   }),
+  GITHUB_CLIENT_ID: Joi.string().required(),
+  GITHUB_CLIENT_SECRET: Joi.string().required(),
+  // host should start with http:// or https://
+  HOST: Joi.string()
+    .pattern(/^(http:\/\/|https:\/\/)/)
+    .required(),
+  CLIENT_HOST: Joi.string()
+    .pattern(/^(http:\/\/|https:\/\/)/)
+    .required(),
+  SESSION_SECRET: Joi.string().required(),
 });
 
 module.exports = schema;
