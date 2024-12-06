a63a1f64e17a7a9599c23eda2c9f4b5579027b4d
Foyzul Karim <foyzulkarim@gmail.com>
2024-12-06 19:29:24 +1100

    delete unwanted modules

--- src/domains/product/index.js
+++
@@ -1,7 +0,0 @@
-const { routes } = require('./api');
-
-const defineRoutes = (expressRouter) => {
-  expressRouter.use('/products', routes());
-};
-
-module.exports = defineRoutes;
--- src/domains/student/index.js
+++
@@ -1,7 +0,0 @@
-const { routes } = require('./api');
-
-const defineRoutes = (expressRouter) => {
-  expressRouter.use('/student', routes());
-};
-
-module.exports = defineRoutes;
